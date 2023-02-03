d15443c6bf605c33d64ba90df7bdd46bd26c1235
gautamrizwani <gautamrizwani@gmail.com>
2023-02-03 15:47:23 +0530

    InitialCommit

---
+++ src/main/java/com/MySpring/DatabaseEx/DatabaseExApplication.java
@@ -0,0 +1,13 @@
+package com.MySpring.DatabaseEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseExApplication.class, args);
+	}
+
+}
